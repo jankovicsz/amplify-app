52ec926c8ce54ae2407d5e5a44ffde1f1c04ba2b
Jankovicsz Zoltan <jankovicsz@gmail.com>
2021-10-26 20:26:34 +0200

    Initialized amplify-app

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import InputFields from './InputFields'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <InputFields />
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/InputFields.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InputFields = () => {
+  return (
+    <div>
+      <h2>Input</h2>
+    </div>
+  );
+};
+
+export default InputFields;
